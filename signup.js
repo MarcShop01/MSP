4eb90617ccf0368a8b7f8ed8964e1a7f8bf1a418
MarcShop01 <marcshop0705@gmail.com>
2025-02-21 21:54:08 -0400

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,16 @@
+document.getElementById("signup-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const nom = document.getElementById("nom").value;
+    const telephone = document.getElementById("telephone").value;
+    const pays = document.getElementById("pays").value;
+    const email = document.getElementById("email").value;
+    const adresse = document.getElementById("adresse").value;
+
+    let utilisateurs = JSON.parse(localStorage.getItem("utilisateurs")) || [];
+    utilisateurs.push({ nom, telephone, pays, email, adresse });
+    localStorage.setItem("utilisateurs", JSON.stringify(utilisateurs));
+    
+    alert("Inscription réussie !");
+    window.location.href = "login.html";
+});
