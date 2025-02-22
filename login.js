65ecc3d5be88e5ff3166b8d0a9849f4b2587086f
MarcShop01 <marcshop0705@gmail.com>
2025-02-22 00:00:55 -0400

    Update login.js

--- login.js
+++ login.js
@@ -2,16 +2,4 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     event.preventDefault();
 
     const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    let utilisateurs = JSON.parse(localStorage.getItem("utilisateurs")) || [];
-    const utilisateur = utilisateurs.find(user => user.email === email && user.password === password);
-
-    if (utilisateur) {
-        localStorage.setItem("utilisateurConnecté", JSON.stringify(utilisateur));
-        alert("Connexion réussie !");
-        window.location.href = "index.html";
-    } else {
-        alert("Email ou mot de passe incorrect.");
-    }
-});
+    const password
