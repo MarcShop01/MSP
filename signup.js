7d3686b3c7c5ccf0a9a06fac420cf0a516c91dbb
MarcShop01 <marcshop0705@gmail.com>
2025-02-21 22:46:24 -0400

    Update signup.js

--- signup.js
+++ signup.js
@@ -6,9 +6,10 @@ document.getElementById("signup-form").addEventListener("submit", function(event
     const pays = document.getElementById("pays").value;
     const email = document.getElementById("email").value;
     const adresse = document.getElementById("adresse").value;
+    const password = document.getElementById("password").value;
 
     let utilisateurs = JSON.parse(localStorage.getItem("utilisateurs")) || [];
-    utilisateurs.push({ nom, telephone, pays, email, adresse });
+    utilisateurs.push({ nom, telephone, pays, email, adresse, password });
     localStorage.setItem("utilisateurs", JSON.stringify(utilisateurs));
     
     alert("Inscription réussie !");
