0939427fbf9bc8215021ec0c4c2e0642ed337a99
MarcShop01 <marcshop0705@gmail.com>
2025-02-22 21:11:50 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -226,14 +226,19 @@ function afficherUtilisateurs() {
     const utilisateursContainer = document.getElementById("utilisateurs-container");
     let utilisateurs = JSON.parse(localStorage.getItem("utilisateurs")) || [];
     utilisateursContainer.innerHTML = "";
+
     if (utilisateurs.length === 0) {
         utilisateursContainer.innerHTML = "<p>Aucun utilisateur inscrit pour le moment.</p>";
         return;
     }
+
     utilisateurs.forEach(utilisateur => {
         let div = document.createElement("div");
         div.classList.add("utilisateur");
         div.innerHTML = `
             <p><strong>Nom:</strong> ${utilisateur.nom}</p>
-            <p;
-    }
+            <p><strong>Email:</strong> ${utilisateur.email}</p>
+        `;
+        utilisateursContainer.appendChild(div);
+    });
+}
