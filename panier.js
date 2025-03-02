2b43657c744f7f8fd684ca9662aea29f1433b094
MarcShop01 <marcshop0705@gmail.com>
2025-03-02 13:52:54 -0400

    Update panier.js

--- panier.js
+++ panier.js
@@ -113,7 +113,8 @@ document.addEventListener("DOMContentLoaded", () => {
             message: `Un utilisateur a laissé un commentaire : "${commentaire}".`,
         };
 
-        emailjs.send("marc1304","template_zvo5tzs"templateParams) // Remplacez par vos IDs EmailJS
+       emailjs.send("marc1304", "template_zvo5tzs", templateParams) // Remplacez par vos IDs EmailJS
+
             .then(response => {
                 console.log("E-mail envoyé !", response.status);
                 alert("Commentaire envoyé avec succès !");
