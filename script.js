23ac8244ad1b02c337c3a2ffce5ff89df888d764
MarcShop01 <marcshop0705@gmail.com>
2025-08-31 13:28:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const SIZE_OPTIONS = {
   shoes: ["36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46"],
   electronics: ["Standard", "Petit", "Moyen", "Grand", "Extra Large"],
   home: ["Petit", "Moyen", "Grand", "Personnalisé"],
-  sports: ["XS", "S", "M", "L", "XL", "XXL"],
+  sports: ["36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46"],
   beauty: ["100ml", "200ml", "250ml", "500ml", "1L"],
   default: ["Unique", "Standard", "Personnalisé"]
 };
@@ -908,3 +908,4 @@ function shareWebsite() {
     });
   }
 } 
+
